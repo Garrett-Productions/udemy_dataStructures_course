d314400676640c8358ec2597a8ddc770368c6251
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 15:19:20 -1000

    first commit

---
+++ day3/day3.js
@@ -0,0 +1,10 @@
+// Linear Time = the amount of time it takes for a function to finish, based # of operations in input
+
+//ES5
+function compressAllBoxes(boxes){
+    boxes.forEach(com(item){
+        console.log(item);
+    });
+}
+
+compressAllBoxes(5)
