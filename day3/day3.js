23dace8863487964fdc39913cdc2b4629df44217
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 15:23:09 -1000

    frist commit

--- day3/day3.js
+++ day3/day3.js
@@ -1,10 +1,18 @@
 // Linear Time = the amount of time it takes for a function to finish, based # of operations in input
 
 //ES5
-function compressAllBoxes(boxes){
-    boxes.forEach(com(item){
-        console.log(item);
-    });
+// function compressAllBoxes(boxes){
+//     boxes.forEach(com(item){
+//         console.log(item);
+//     });
+// }
+
+// compressAllBoxes(5)
+
+//ES6
+const compressAllBoxes = boxes => {
+    boxes.forEach(box => console.log(box));
 }
 
-compressAllBoxes(5)
+var box = compressAllBoxes(5);
+console.log(box)
