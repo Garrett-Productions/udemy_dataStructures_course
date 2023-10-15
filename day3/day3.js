f80dcab8e8cce7497aa812d1121d56962eb07785
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 15:31:59 -1000

    frist commit

--- day3/day3.js
+++ day3/day3.js
@@ -16,3 +16,15 @@ const compressAllBoxes = boxes => {
 
 var box = compressAllBoxes(5);
 console.log(box)
+
+function funChallenge(input){
+    let a = 10;
+    a = 50 + 3;
+    for(let i = 0; i < input.length; i++){
+        anotherFunction();
+        let stranger = true;
+        a++;
+    }
+    return a;
+}
+funChallenge();
