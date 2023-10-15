f81120982af9ad9e3e749d4d79040564b19ace07
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 15:10:54 -1000

    frist commit

--- day2/day2.js
+++ day2/day2.js
@@ -2,7 +2,7 @@ const nemo = ['nemo']
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla']
 const largeArray = new Array(100).fill('nemo')
 const largerArray = new Array(1000).fill('nemo')
-const evenLargerArray = new Array(1000).fill('nemo')
+const evenLargerArray = new Array(10000).fill('nemo')
 const largestArray = new Array(100000).fill('nemo')
 
 function findNemo(array){
@@ -17,3 +17,8 @@ function findNemo(array){
 }
 
 findNemo(largestArray)
+findNemo(largeArray)
+// When we are talking about big(0), and scalability of code, we simply mean when we grow bigger and bigger with our input
+// how much does our algorithm slow down? The less it slows down the better our algorithm is.
+// Play around with the variables above to plug them into to the function call and check out your CPU's run time
+// Using JS built ins, array.fill() and performance.now()
