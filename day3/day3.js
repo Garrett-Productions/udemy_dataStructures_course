53176986415dee85cdf882bb5e84ee4a0725991f
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-14 15:34:45 -1000

    frist commit

--- day3/day3.js
+++ day3/day3.js
@@ -18,14 +18,14 @@ var box = compressAllBoxes(5);
 console.log(box)
 
 function funChallenge(input){
-    let a = 10; // 0(1)
-    a = 50 + 3; // 0(1)
+    let a = 10; // 0(1) one step
+    a = 50 + 3; // 0(1) two step
     for(let i = 0; i < input.length; i++){
         anotherFunction(); // we dont know this, its called based on how big our input is
         let stranger = true; // runs based on input, or, 0(n)
         a++; // 53 becomes 54. it runs 0(n)
     }
-    return a; //runs once, 0(1)
+    return a; //runs once, 0(1) 3 step
 }
 // 3 steps + 0(n) + 0(n) + 0(n)
 3 + 
