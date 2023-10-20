3522535e740e3bdaa97895bc1dd294d41e036ba2
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-20 13:14:23 -1000

    first commit

--- arrays/arrays.day5.js
+++ arrays/arrays.day5.js
@@ -21,5 +21,20 @@ const stringsExShow =
 
 // perhaps arrays aren't the best data structure to use if you're gonna need to add items to the beginning of the array
 
-
+//What about adding to the middle of the array?
 //What about Splice?
+
+strings.splice() 
+// splice takes a start number, or index of the array, the 2nd param is how many values to delete from there,
+// the 3rd value is the value to add to the array
+
+// when hovering over
+// (method) Array<string>.splice(start: number, deleteCount?: number | undefined): string[] (+1 overload)
+// Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+
+// @param start — The zero-based location in the array from which to start removing elements.
+
+// @param deleteCount — The number of elements to remove.
+
+// @returns — An array containing the elements that were deleted.
+
