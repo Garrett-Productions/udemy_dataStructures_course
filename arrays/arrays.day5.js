1b1faa0f497d540072f77a9677806e940179938c
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-20 13:04:52 -1000

    first commit

--- arrays/arrays.day5.js
+++ arrays/arrays.day5.js
@@ -2,3 +2,10 @@ const strings = ["a", "b", "c", "d"]
 // 4*4 = 16 bytes of storage
 // we stored it, lets grab it
 console.log(strings[2]) // hey you stored a DS in memory called strings which is an array. grab the 3rd value or 2nd index from this array
+
+// push 
+strings.push('e')
+console.log(strings)
+// because we are just adding to the end, its an O(1) operation, we arent looping, we are just going through it one time.
+strings.pop();
+console.log(strings)
