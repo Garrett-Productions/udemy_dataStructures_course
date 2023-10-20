e849a562cf774fb58d3cc548af7388768b0e76f8
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-20 13:08:56 -1000

    first commit

--- arrays/arrays.day5.js
+++ arrays/arrays.day5.js
@@ -4,10 +4,13 @@ const strings = ["a", "b", "c", "d"]
 console.log(strings[2]) // hey you stored a DS in memory called strings which is an array. grab the 3rd value or 2nd index from this array
 
 // push 
-strings.push('e')
+strings.push('e') // O(1), only 1 operation
 console.log(strings)
 // because we are just adding to the end, its an O(1) operation, we arent looping, we are just going through it one time.
 strings.pop();
 console.log(strings)
-strings.pop();
+strings.pop(); //  0(1)
+console.log(strings)
+
+strings.unshift('x') // O(n) have to move all values in the array and insert a new [0] value
 console.log(strings)
