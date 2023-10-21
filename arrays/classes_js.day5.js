7abe6743371567b23374692961ae5c010230be2b
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-20 15:48:02 -1000

    first commit

--- arrays/classes_js.day5.js
+++ arrays/classes_js.day5.js
@@ -6,6 +6,32 @@ console.log(object1 === object3);
 console.log(object1.value =15);
 console.log(object2.value);
 console.log(object3.value);
-
 // With objects we are always referncing, referencing other variables
 // alot of times we get reference type errors.
+const object4 = {
+    a:function(){
+        console.log(this)
+    }
+}
+class Player {
+    constructor(name, type){
+        console.log('player', this);
+        this.name = name;
+        this.type = type;
+    }
+    introduce(){
+        console.log(`Hi I am ${this.name},I'm a ${this.type}`)
+    }
+}
+
+class Wizard extends Player {
+    constructor(name, type){
+        super(name, type)
+        console.log('wizard', this);
+    }
+    play(){
+        console.log(`Weeee I'm a ${this.type}`)
+    }
+}
+const wizardKing = new Wizard('Garrett', 'Level 99');
+const WizardQueen = new Wizard('Charisma', 'Level 98')
