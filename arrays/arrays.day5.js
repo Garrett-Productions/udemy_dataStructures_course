0f2077bbe692248f4ea34012337755dd114353f0
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-20 15:08:29 -1000

    first commit

--- arrays/arrays.day5.js
+++ arrays/arrays.day5.js
@@ -1,6 +1,5 @@
 const strings = ["a", "b", "c", "d"]
-// 4*4 = 16 bytes of storage
-// we stored it, lets grab it
+
 console.log(strings[2]) // hey you stored a DS in memory called strings which is an array. grab the 3rd value or 2nd index from this array
 
 // push 
