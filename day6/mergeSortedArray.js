e96f9b97c98cc4cfbb068bc244ba6c305d6f8f4b
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-21 17:38:40 -1000

    first commit

--- day6/mergeSortedArray.js
+++ day6/mergeSortedArray.js
@@ -15,14 +15,18 @@ function mergeSortedArray(array1, array2){
         return array1 // because its already merged;
     }
     while(arrayItem1 || arrayItem2){ // while these values exist
-        if (arrayItem1 < arrayItem2){ // if the first var is less than
+        if (!arrayItem2 || arrayItem1 < arrayItem2){ // if the first var is less than
             mergedArray.push(arrayItem1) // push it to the new array instance
-            arrayItem1 = array1[i+1] // we dont need to worry about the value of 0 anymore, so set it equal to the next value in the array
+            arrayItem1 = array1[i] // we dont need to worry about the value of 0 anymore, so set it equal to the next value in the array
+            i++ 
         } //  we arent using a for loop so we innitiate i as 1 up top
         else {
-            mergedArray.push(arrayItem2){
-                arrayItem2 = array2[j+1]
-            }
+            mergedArray.push(arrayItem2);
+            arrayItem2 = array2[j]
+            j++
         }
     }
+    return mergedArray;
 }
+
+console.log(mergeSortedArray([0,3,23,10],[2,98,3,2]))
