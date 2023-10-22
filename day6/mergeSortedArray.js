428cdceece3e09f22132778715172709a0e87496
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-21 17:17:43 -1000

    first commit

---
+++ day6/mergeSortedArray.js
@@ -0,0 +1,10 @@
+function mergeSortedArray(array1, array2){
+    const mergedArray=[]
+    let arrayItem1 = array1[0]
+    let arrayItem2 = array2[0];
+
+    //esdge cases, check inputs and input types
+    if(array1.length === 0 ){
+        return array2 // because its already merged
+    }
+}
