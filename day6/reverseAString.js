8c31ee0be93a9ad4621d1491d1962d4ed961ab3b
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-21 15:33:16 -1000

    first commit

--- day6/reverseAString.js
+++ day6/reverseAString.js
@@ -1,1 +1,9 @@
 // We can treat a string just like an array an access it by index, and run built ins like we can on an array
+
+// first check if its a string
+// second, create a var as an empty array thats gonna house all my reversed values
+// third, create a variable equal to the string minus 1 
+// fourth, loop through that string and decrement i every time 
+// fifth, in the code block, .push each char or index to our array var.
+// sixth, console log our array var for assurance, and return our array as a string by .join
+function reverse(str)
