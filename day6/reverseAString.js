2acc7bef395ee1397787c65fa8316f195f5a50c9
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-21 15:56:16 -1000

    first commit

--- day6/reverseAString.js
+++ day6/reverseAString.js
@@ -29,4 +29,10 @@ reverse("Hi my name is Andrei")
 function reverse2(str){
     return str.split('').reverse().join('');
 }
-console.log(reverse2("Hi my name is Andrei"));
+console.log(reverse2("Hi my name is Andrei")); 
+
+// now with ES6
+
+const reverse3 = str => [...str].reverse().join('');
+
+console.log(reverse3("Hi my name is Andrei"))
