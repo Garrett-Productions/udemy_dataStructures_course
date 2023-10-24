418e7929d439f8559c3ca506c50765ade0dea800
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-24 09:07:41 -1000

    frist commit

--- hashTables/day1.hash.js
+++ hashTables/day1.hash.js
@@ -25,6 +25,13 @@ const newSet = Set() // only stores the keys, no values
 class HashTable {
     constructor(size){
         this.data = new Array(size);
-        
+    }
+    _hash(key) {
+        let hash = 0
+        for(let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) %
+            this.data.length
+        }
+        return hash;
     }
 }
