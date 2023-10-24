8640a5fd9cf4589b5b2699e81a798eace8820158
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-24 08:59:26 -1000

    frist commit

--- hashTables/day1.hash.js
+++ hashTables/day1.hash.js
@@ -12,3 +12,6 @@ user.spell = "abra kadabra" //O(1)
 user.scream(); // O(1)
 
 // this is BigO(n/k) k being the size of our hash table, but since we remove constraints, its just bigO(n)
+
+// With a MAP function, it allows you to save any data type as the key,
+// with an object, a key must be a string
