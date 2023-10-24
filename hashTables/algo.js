0bd9365a24c41fb1faab05351c19a23157f9fbe3
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-24 10:37:04 -1000

    frist commit

--- hashTables/algo.js
+++ hashTables/algo.js
@@ -1,1 +1,17 @@
 // first recurring character
+
+// we can brute force something with a nested for loop
+//Given an array = [2,1,7,4,3,2]:
+// it should return 2
+
+function firstRecurringChar(input){
+    for(let i = 0; i <input.length; i++){
+        for(let j = i+1; j <input.length; j++){
+            if(input[i] === input[j]){
+                return input[i];
+            }
+        }
+    }
+    return undefined
+}
+console.log(firstRecurringChar([2,5,1,2,3,5,1,2,4]));
