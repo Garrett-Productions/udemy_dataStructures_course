70f2d6760d3af757f45b530b8ee4b7735e04fc22
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-24 09:31:43 -1000

    frist commit

--- hashTables/day1.hash.js
+++ hashTables/day1.hash.js
@@ -36,6 +36,14 @@ class HashTable {
         }
         return hash;
     }
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value])
+        return this.data
+    }
 }
 const myHashTable = new HashTable(50)
 console.log(myHashTable)
