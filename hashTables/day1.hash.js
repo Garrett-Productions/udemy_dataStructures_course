4fd31738257aa709576418fd14669463329211c5
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-24 09:20:53 -1000

    frist commit

--- hashTables/day1.hash.js
+++ hashTables/day1.hash.js
@@ -31,6 +31,8 @@ class HashTable {
         for(let i = 0; i < key.length; i++){
             hash = (hash + key.charCodeAt(i) * i) % // this gives us a random num between 0-65,000 for every char in our string, times it by itself, and then runs a modulo to keep it within the length
             this.data.length
+            console.log(hash)
+            console.log(key.charCodeAt())
         }
         return hash;
     }
--- hashTables/tempCodeRunnerFile.js
+++ hashTables/tempCodeRunnerFile.js
@@ -5,8 +5,10 @@ class HashTable {
     _hash(key) {
         let hash = 0
         for(let i = 0; i < key.length; i++){
-            hash = (hash + key.charCodeAt(i) * i) %
+            hash = (hash + key.charCodeAt(i) * i) % // this gives us a random num between 0-65,000 for every char in our string, times it by itself, and then runs a modulo to keep it within the length
             this.data.length
+            console.log(hash)
+            console.log(key.charCodeAt())
         }
         return hash;
     }
@@ -14,5 +16,3 @@ class HashTable {
 const myHashTable = new HashTable(50)
 console.log(myHashTable)
 myHashTable._hash('grapes');
-myHashTable.set('grapes', 100000)
-console.log(myHashTable)
