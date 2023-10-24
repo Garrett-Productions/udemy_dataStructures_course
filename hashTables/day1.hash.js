25436ba96580ca21d9aedf0942ccfafbf11b6db0
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-10-24 08:49:07 -1000

    frist commit

---
+++ hashTables/day1.hash.js
@@ -0,0 +1,12 @@
+let user = {
+    age: 54,
+    name:'John',
+    magic: true,
+    scream: function(){
+        console.log('ahhhh!')
+    }
+}
+
+console.log(user.age)
+user.spell = "abra kadabra"
+user.scream();
