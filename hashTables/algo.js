ef1df8fd8d756303a12543faa2f8b1e96ea63181
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-11-16 15:25:10 -1000

    first commit

--- hashTables/algo.js
+++ hashTables/algo.js
@@ -42,6 +42,14 @@ function recurringCharacter(array){
     // Return the first character we see twice
     // we can brute force this with a nested for loop
     // we can have two pointers that make comparisons towards one another, and if they equal one another, return the int's
+    for(i = 0; i < array.length; i++){
+        for (j=i+1; j < array.length; j++){
+            if(array[i] === array[j]){
+                return console.log("Array of i is:", array[i], "Array of j is:", array[j])
+            }
+        }
+    }
+    return undefinded
 }
 
 console.log(recurringCharacter([2,8,3,8,6,5]))
